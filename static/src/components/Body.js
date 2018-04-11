3e8bfe54718d901d740b7e469dc43e442e560204
Kerry-G <kerrygougeon@gmail.com>
2018-04-11 02:24:24 -0400

    change app into static

---
+++ static/src/components/Body.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+import Form from 'muicss/lib/react/form';
+import Input from 'muicss/lib/react/input';
+import Select from 'muicss/lib/react/select';
+import Option from 'muicss/lib/react/option';
+
+class Body extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            name:"camille",
+            amount:"",
+            startDate: moment(),
+            type:"grocery",
+        };
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleChangeDate = this.handleChangeDate.bind(this);
+        this.handleChangeAmount = this.handleChangeAmount.bind(this);
+    }
+
+    handleChangeDate(date) {
+        this.setState({
+            startDate: date
+        });
+        console.log(date)
+    }
+
+    handleChangeAmount(e){
+        const amount = e.target.value
+        this.setState({amount})
+    }
+    handleSubmit(e){
+        e.preventDefault()
+        this.defaultState()
+        console.log(this.state)
+    }
+
+    defaultState(){
+        this.setState({
+            name:"camille",
+            amount:"",
+            startDate: moment(),
+            type:"grocery"
+        })
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <Form>
+                <Select 
+                    label="Name" 
+                    defaultValue="Camille" 
+                    onChange={e=>{this.setState({name:e.target.value})}}
+                >
+                    <Option value="camille" label="Camille"/>
+                    <Option value="kerry"   label="Kerry"  />
+                </Select>
+                
+                <Select 
+                    label="type"
+                    onChange={e=>{this.setState({type:e.target.value})}}
+                >
+                    <Option value="grocery"  label="Grocery"  />
+                    <Option value="pharmacy" label="Pharmacy" />
+                    <Option value="other"    label="Option"   />
+                </Select>
+
+                <Input floatingLabel label="Amount" type="number" value={this.state.amount} 
+                onChange={this.handleChangeAmount}/>
+  
+
+                <div className="datepicker">
+                <DatePicker
+                    inline
+                    selected={this.state.startDate}
+                    onChange={this.handleChangeDate}
+                />
+                </div>
+
+                <div className="button-container">
+                    <button className="submit" type="submit" onClick={this.handleSubmit}>Save</button>
+                </div>
+                </Form>
+            </div>
+        );
+    }
+}
+
+export default Body;
--- static/src/index.js
+++ static/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles.css';
+
+import 'react-datepicker/dist/react-datepicker.css';
+
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
